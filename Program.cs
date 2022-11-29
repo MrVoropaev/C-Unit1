bf13f2a3e205c952f13c888cab210ae58f5a3c0f
MrVoropaev <mrvoropaev@gmail.com>
2022-11-29 20:44:15 +0300

    2 Zadacha

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿Console.WriteLine("Введите число");
-string numberstr = Console.ReadLine() ?? "";
-int number = Convert.ToInt32(numberstr);
+﻿string input0 = Console.ReadLine() ?? "";
+string input1 = Console.ReadLine() ?? "";
+int num0 = Convert.ToInt32(input0);
+int num1 = Convert.ToInt32(input1);
 
-int result = number * number;
-
-Console.WriteLine($"Квадрат числа {number} = {result}");
+if ((num1 * num0!) == num0)
+{
+    Console.WriteLine($"Число {num1} является квадратным корнем числа {num0}");
+}
+else
+{
+    Console.WriteLine($"число {num1} не является квадратным корнем числа {num0}");
+}
