4b41cc3ec7701dbfc3e188ab6cbc9a55c7f13704
MrVoropaev <mrvoropaev@gmail.com>
2022-11-29 21:29:56 +0300

    5.1  Zadacha

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
-﻿Console.WriteLine("Введите число");
+﻿/*Console.WriteLine("Введите число");
 string nstring = Console.ReadLine() ?? "";
 int n = Convert.ToInt32(nstring);
 
-Console.WriteLine(n % 10);
+Console.WriteLine(n % 10);*/
+
+string nstring = Console.ReadLine() ?? "";
+Console.WriteLine(nstring[2]);
+
