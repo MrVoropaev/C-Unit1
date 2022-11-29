02226eaa5fa802e10ff0a1a0dbb0e00b36b048ca
MrVoropaev <mrvoropaev@gmail.com>
2022-11-29 20:34:51 +0300

    1 Zadacha

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+string numberstr = Console.ReadLine() ?? "";
+int number = Convert.ToInt32(numberstr);
+
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} = {result}");
