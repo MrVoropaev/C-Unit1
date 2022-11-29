1733bdd08e649c3d78c6bc188d0bc238c0bb7c41
MrVoropaev <mrvoropaev@gmail.com>
2022-11-29 21:02:46 +0300

    3.2 Zadacha

--- Program.cs
+++ Program.cs
@@ -1,35 +1,31 @@
 ﻿Console.WriteLine("Input");
 string day = Console.ReadLine() ?? "";
 
-if (day == "1")
+switch (day)
 {
+    case "1":
     Console.WriteLine("Понедельник");
-}
-else if (day == "2")
-{
+    break;
+    case "2":
     Console.WriteLine("Вторник");
-}
-else if (day == "3")
-{
+    break;
+    case "3":
     Console.WriteLine("Среда");
-}
-else if (day == "4")
-{
+    break;
+    case "4":
     Console.WriteLine("Четверг");
-}
-else if (day == "5")
-{
+    break;
+    case "5":
     Console.WriteLine("Пятница");
-}
-else if (day == "6")
-{
+    break;
+    case "6":
     Console.WriteLine("Суббота");
-}
-else if (day == "7")
-{
-    Console.WriteLine("Воскресенье");
-}
-else
-{
+    break;
+    case "7":
+    Console.WriteLine("Воскресение");
+    break;
+    default:
     Console.WriteLine("Wrong");
+    break;
+
 }
