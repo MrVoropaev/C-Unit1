c3ecbe6e3a4e84864da4ce5a16c3cf088919eff5
MrVoropaev <mrvoropaev@gmail.com>
2022-12-02 20:40:22 +0300

    U2Z3

---
+++ Unit2/Z2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int GetRandomNumber()
+{
+    
+    return new Random().Next(100,1000);
+}
+//Console.WriteLine(GetRandomNumber());
+
+void ShowRandomNumber(int number)
+{
+    Console.WriteLine($"Число: {number}");
+    string str = number.ToString();
+    Console.WriteLine($">{str[0]}{str[2]}");
+}
+
