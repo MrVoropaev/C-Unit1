78545ae6596ddbed16ae2bd4ac17db0a05ad61c8
MrVoropaev <mrvoropaev@gmail.com>
2022-12-06 20:20:13 +0300

    U2Z7

---
+++ Unit2/Z7/Program.cs
@@ -0,0 +1,38 @@
+﻿int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result) && result != 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели некорректное число");
+        }
+    }
+
+    return result;
+}
+
+int GetQuarter(int x, int y)
+{
+    if(x > 0 && y > 0)
+        return 1;
+    if(x < 0 && y > 0)
+        return 2;
+    if(x < 0 && y < 0)
+        return 3;
+
+    return 4;
+}
+
+int x = GetNumber("Введите координату х:");
+int y = GetNumber("Введите координату y:");
+int quarter = GetQuarter(x, y);
+
+Console.WriteLine($"Точка с координатами ({x}, {y}) лежит в {quarter} четверти");
