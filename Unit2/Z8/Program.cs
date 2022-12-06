814a2328f56e6e08f37b9cbdab2c449b9fe3df04
MrVoropaev <mrvoropaev@gmail.com>
2022-12-06 21:10:23 +0300

    U2Z8

---
+++ Unit2/Z8/Program.cs
@@ -0,0 +1,13 @@
+﻿int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = ReadNumber("Введи число: ");
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write(Math.Pow(i, 2) + " ");
+    //Console.Write($"{i*i*i} ");//
+}
