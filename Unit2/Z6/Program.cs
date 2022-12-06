b615bd5fb0718cdce9d8c3ba768ad504ed4156ed
MrVoropaev <mrvoropaev@gmail.com>
2022-12-06 19:49:59 +0300

    U2Z6

---
+++ Unit2/Z6/Program.cs
@@ -0,0 +1,15 @@
+﻿int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 * number1 == number2)
+{
+    Console.WriteLine("Первое число является квадратом второго ");
+}
+else if (number2 * number2 == number1)
+{
+    Console.WriteLine("Второе число является квадратом первого ");
+}
+else
+{
+    Console.WriteLine("Числа не являются квадратми друг относительно друга");
+}
