651d6a1d2452269d70a75b13c1d41640d6ea98ed
MrVoropaev <mrvoropaev@gmail.com>
2022-12-13 21:14:56 +0300

    Unit2

--- Unit2/Program.cs
+++ Unit2/Program.cs
@@ -1,6 +1,15 @@
-﻿void CompareNums(int x, int y)
+﻿int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 * number1 == number2)
 {
-    if (x % y == 0) Console.WriteLine("Число 1 кратно числу 2");
-    else Console.WriteLine(x % y);
+    Console.WriteLine("Первое число является квадратом второго ");
 }
-CompareNums(16, 4);
+else if (number2 * number2 == number1)
+{
+    Console.WriteLine("Второе число является квадратом первого ");
+}
+else
+{
+    Console.WriteLine("Числа не являются квадратми друг относительно друга");
+}
